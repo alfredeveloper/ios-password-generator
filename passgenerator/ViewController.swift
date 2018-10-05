9b67576d2c1bd49ba83d39bbdf2a125a4dcd3c71
Alfredo Yupanqui Alarcon <alfreddeveloper@alfreds-MacBook-Air.local>
2018-10-04 22:49:09 -0500

    Initial Commit

---
+++ passgenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passgenerator
+//
+//  Created by Alfredo Yupanqui Alarcon on 10/4/18.
+//  Copyright © 2018 alfredeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
