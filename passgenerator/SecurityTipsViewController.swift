818d7d1fc4894e7e2608f945d5eb17116d8c93a6
Alfredo Yupanqui Alarcon <alfreddeveloper@alfreds-MacBook-Air.local>
2018-10-06 12:37:52 -0500

    project iOS password generator

---
+++ passgenerator/SecurityTipsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecurityTipsViewController.swift
+//  passgenerator
+//
+//  Created by Alfredo Yupanqui Alarcon on 10/5/18.
+//  Copyright © 2018 alfredeveloper. All rights reserved.
+//
+
+import UIKit
+
+class SecurityTipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
